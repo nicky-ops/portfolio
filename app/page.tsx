7af46978bcbf6e752260bf593cde21d9984389af
Nickson Kipruto Rop <nickyrutto@gmail.com>
2024-07-27 22:52:08 +0300

    register Approach and Footer sections

--- app/page.tsx
+++ app/page.tsx
@@ -7,7 +7,7 @@ import RecentProjects from "@/components/RecentProjects";
 import { navItems } from "@/data";
 import Clients from "@/components/Clients";
 import Experience from "@/components/Experience";
-import Approach from "@/components/Approach";
+import { Approach } from "@/components/Approach";
 
 export default function Home() {
   return (
@@ -20,6 +20,7 @@ export default function Home() {
       <Clients />
       <Experience />
       <Approach />
+      <Footer />
     </div>
    </main>
   );
