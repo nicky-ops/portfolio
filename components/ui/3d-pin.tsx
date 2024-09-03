852a195a173958b153c4ae8c717abf904347621f
Nickson Kipruto Rop <nickyrutto@gmail.com>
2024-09-03 23:23:52 +0300

    fix errors

--- components/ui/3d-pin.tsx
+++ components/ui/3d-pin.tsx
@@ -1,8 +1,11 @@
 "use client";
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { motion } from "framer-motion";
 import { cn } from "@/utils/cn";
 import Link from "next/link";
+import dynamic from "next/dynamic" 
+
+const ClientSidePinPerspective = dynamic(() => Promise.resolve(PinPerspective), {ssr: false,});
 
 export const PinContainer = ({
   children,
@@ -17,9 +20,10 @@ export const PinContainer = ({
   className?: string;
   containerClassName?: string;
 }) => {
-  const [transform, setTransform] = useState(
-    "translate(-50%,-50%) rotateX(0deg)"
-  );
+  const [transform, setTransform] = useState("");
+  useEffect(() => {
+    setTransform("translate(-50%,-50%) rotateX(0deg)")
+  }, []);
 
   const onMouseEnter = () => {
     setTransform("translate(-50%,-50%) rotateX(40deg) scale(0.8)");
@@ -39,6 +43,7 @@ export const PinContainer = ({
       href={href || "/"}
     >
       <div
+        suppressHydrationWarning
         style={{
           perspective: "1000px",
           transform: "rotateX(70deg) translateZ(0deg)",
@@ -54,7 +59,7 @@ export const PinContainer = ({
           <div className={cn(" relative z-50 ", className)}>{children}</div>
         </div>
       </div>
-      <PinPerspective title={title} href={href} />
+      <ClientSidePinPerspective title={title} href={href} />
     </Link>
   );
 };
@@ -70,17 +75,11 @@ export const PinPerspective = ({
     <motion.div className="pointer-events-none  w-full h-80 flex items-center justify-center opacity-0 group-hover/pin:opacity-100 z-[60] transition duration-500">
       <div className=" w-full h-full -mt-7 flex-none  inset-0">
         <div className="absolute top-0 inset-x-0  flex justify-center">
-          <a
-            href={href}
-            target={"_blank"}
-            className="relative flex space-x-2 items-center z-10 rounded-full bg-zinc-950 py-0.5 px-4 ring-1 ring-white/10 "
-          >
             <span className="relative z-20 text-white text-xs font-bold inline-block py-0.5">
               {title}
             </span>
 
             <span className="absolute -bottom-0 left-[1.125rem] h-px w-[calc(100%-2.25rem)] bg-gradient-to-r from-emerald-400/0 via-emerald-400/90 to-emerald-400/0 transition-opacity duration-500 group-hover/btn:opacity-40"></span>
-          </a>
         </div>
 
         <div
