6adc672ca19dbbff641426c1c50d492b94163301
Nickson Kipruto Rop <nickyrutto@gmail.com>
2024-09-03 22:15:30 +0300

    modify list of clients

--- components/Clients.tsx
+++ components/Clients.tsx
@@ -1,7 +1,6 @@
 import React from 'react'
 import { InfiniteMovingCards } from './ui/InfiniteMovingCards'
 import { companies, testimonials } from '@/data'
-import { div } from 'three/examples/jsm/nodes/Nodes.js'
 
 const Clients = () => {
   return (
@@ -17,10 +16,9 @@ const Clients = () => {
                 speed="slow"
               />
               <div className='flex flex-wrap items-center justify-center gap-4 md:gap-16 max-lg:mt-10'>
-                {companies.map(({id, img, name, nameImg}) => (
+                {companies.map(({id, img, name}) => (
                   <div key={id} className="flex md:max-w-60 max-w-32 gap-2">
                     <img src={img} alt={name} className="md:w-10 w-5" />
-                    <img src={nameImg} alt={name} className="md:w-24 w-20" />
                   </div>
                 ))}
               </div>
